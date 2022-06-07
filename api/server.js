bb1686ba4f5ddbdf06cc7f2d328313589f487a54
unknown <lorenzo.lgayle@gmail.com>
2022-06-07 16:34:51 -0400

    boiler plate added

--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const Posts = require('./posts/posts-model');
+
+const postsRouter = require('./posts/posts-router');
+
+server.use('/api/posts', postsRouter);
+
+module.exports = server;
